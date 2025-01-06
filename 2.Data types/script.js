3eadfc6f4dd72aa09978275779908d6693785167
Hamid <hamid@ahmadalisheikh267.com>
2025-01-06 21:12:10 +0500

    Data types in JavaScript

---
+++ 2.Data types/script.js
@@ -0,0 +1,25 @@
+
+              // Data Types
+           // Boolean
+           let JavaScript = true;
+           console.log(typeof true)
+           console.log(typeof JavaScript);
+                      // String
+           let lastName = 'Bhai';
+           console.log(typeof lastName);
+                    // number
+           console.log(typeof 23);
+                 // Reassign value
+           JavaScript = 'YES!';
+           console.log(typeof JavaScript)
+           
+                      // undefined
+           let year;
+           console.log(year);
+           console.log(typeof year);
+                 // Reassign value
+           year = 2006;
+           console.log(typeof year);
+                  // null
+           console.log(typeof null);
+           
