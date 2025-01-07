3b03e947af5db9576fdcace717d9caf57523d82b
Hamid <hamid@ahmadalisheikh267.com>
2025-01-07 21:43:46 +0500

    Value and Variables in JavaScript

---
+++ 1.Value and variables/script.js
@@ -0,0 +1,131 @@
+let js = 'amazing';
+// if(js === 'amazing') alert("JavaScript is FUN!");
+
+console.log(40+21+10-23);
+
+
+             // Value and Variables
+console.log('hamid');   
+
+let firstName = 'hamid';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+let PI = 3.433;
+
+// Variables name are like that
+let myCurrentjob = 'Electrion';
+let myFirstjob = 'School';
+let my_name = 'hamid';
+
+// Variable names are writen not like that
+let name = 'hamid';
+// let 3years = 'awais';
+let fuction = 'wedding';
+
+
+              // Data Types
+           // Boolean
+let JavaScript = true;
+console.log(typeof true)
+console.log(typeof JavaScript);
+           // String
+let lastName = 'Bhai';
+console.log(typeof lastName);
+         // number
+console.log(typeof 23);
+      // Reassign value
+JavaScript = 'YES!';
+console.log(typeof JavaScript)
+
+           // undefined
+let year;
+console.log(year);
+console.log(typeof year);
+      // Reassign value
+year = 2006;
+console.log(typeof year);
+       // null
+console.log(typeof null);
+
+         // let const and var
+let age = 18;
+age = 19;
+
+         // const
+const birthday = 2006;
+// In const we cannot reassigning variables
+      // var
+var ages = 'same as let but different';
+
+
+
+                     // Basic operators
+         // Math Operator
+const ageHamid = 2024 - 2006
+console.log(ageHamid);
+
+const ageAwais = 2024 - 2003
+const ageAsim = 2024 - 2008
+console.log(ageAwais, ageAsim)
+
+// Here we replace now  with 2024 because it same 
+const now = 2024
+const ageAhmad = now - 2003;
+const ageAli = now - 2008;
+console.log(ageAhmad, ageAli , ageAhmad * 2 , ageAli / 2, 2 ** 3);
+// 2 ** 3  means 2  to the  power of 3 is 2 * 2 * 2  is 8
+
+let ali = 18
+let hamid = 20
+console.log(ali + hamid);
+
+let firstName1 = 'Hamid';
+let lastName2 = 'Bhai';
+console.log(firstName1 + lastName2)
+console.log(firstName1 + ' ' + lastName2)
+// In this console we give  space between names
+
+
+
+              //Assingment operator
+let x = 10 +5 ;//15
+x += 10 ;//x+=10 means x = x +10 = 25
+x *= 4; //x = x * 4 = 100
+x++; // x = x 1
+x--;
+x--;
+console.log(x);
+
+
+             //Comparisan operator
+//Its perform boolean value
+
+//The value of ageAhmad and ageAli are given up
+console.log(ageAhmad > ageAli);
+console.log(ageAhmad < ageAli);
+console.log(ageAhmad >= 20);
+console.log(ageAhmad <= 20);
+
+//Same as upper 107 line
+console.log(2024 - 2003 > 2024 - 2008)
+
+
+
+
+const noww = 2024
+const ageAbrar = noww - 2003;
+const ageAbbas = noww - 2008;
+
+console.log(noww - 2003 > noww - 2008);
+
+let y , z;
+y = z = 25 - 10 - 5;// Here y = z = 10 , y = 10 and y = z
+console.log(y , z);
+
+
+                        // Grouping
+const averAge = (ageAhmad + ageAsim) /2 ;
+console.log( ageAhmad , ageAsim ,averAge);
