e88810c935c222e5c47cf39573d7037f73c10985
Hamid <hamid@ahmadalisheikh267.com>
2025-01-15 09:15:12 +0500

    Equality operator  in JavaScript

---
+++ 7.Equality operator ===/script.js
@@ -0,0 +1,18 @@
+const age = 18;
+if(age === 18 ) console.log('You just become an adult :D!')
+
+const favourite  = Number(prompt('What is tour favourite color?'))  ;
+console.log(favourite);
+
+
+if (favourite === 23) console.log('Cool! 23 is amazing number');
+else if (favourite === 9)  { console.log('Cool! 7 is also a amazing number') ;
+}
+
+    else if(favourite == 7){// == is a loose operator so we use === because it is a strick operator .IN '18' == 18 But IN  '18' is not === 18.
+    console.log('Cool! 7 is also a amazing number');
+    }
+    else{
+        console.log('Number is not equal to 23, 9 , 7');s
+    }
+    if (favourite!== 23) console.log('Why? not 23?');
